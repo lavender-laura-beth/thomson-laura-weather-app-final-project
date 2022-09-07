25941910732c4a1a2acbc8d01d523728c176f13f
lavender-laura-beth <111800323+lavender-laura-beth@users.noreply.github.com>
2022-09-06 22:08:11 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -112,7 +112,7 @@ function cityTemperature(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
-  iconElement.setAttribute("alt", response.data.weather[0]).description;
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 
   getForecast(response.data.coord);
 }
