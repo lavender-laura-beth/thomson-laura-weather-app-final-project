b268b10bcd53b95434fda972fc28e0d444d7f915
lavender-laura-beth <111800323+lavender-laura-beth@users.noreply.github.com>
2022-09-06 21:47:45 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -36,15 +36,7 @@ h4.innerHTML = `${day}, ${month} ${date} | ${hours}:${minutes} `;
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
-  let days = [
-    "sunday",
-    "monday",
-    "tuesday",
-    "wednesday",
-    "thursday",
-    "friday",
-    "saturday",
-  ];
+  let days = ["sun", "mon", "tues", "wed", "thurs", "fri", "sat"];
   return days[day];
 }
 
