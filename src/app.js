e6cda78f4bff4e922a5d5fdae2fc98dfdf5622e5
lavender-laura-beth <111800323+lavender-laura-beth@users.noreply.github.com>
2022-09-06 22:55:14 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -88,7 +88,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   console.log(coordinates);
-  let apiKey = "73ff2a59c2dc5fd1e27fc5c64c2da1b5";
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayForecast);
 }
